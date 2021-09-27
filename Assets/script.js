d4d30b99b1b8ff9876245a7365aaa0bd12f31168
Faizila Amlani <amlani.faizila@gmail.com>
2021-09-27 09:11:25 -0500

    updated all files

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,22 @@
-// Define Variables
-var startButton = document.querySelector(".start");
-var timerEl = document.querySelector(".timer");
-var questionsList = document.getElementById("questionsList");
-var highscores = document.querySelector(".highscores");
+// Declare Variables
+var timerEl = document.getElementById("timer");
+var startBox = document.getElementById("start-box");
+var startButton = document.getElementById("start-quiz");
+var questionBox = document.getElementById("questions-box");
+var finish = document.getElementById("finish-box");
+var quizQuestionEl = document.getElementById("quiz-question");
+var optionButtons = document.getElementById("option-buttons");
+var correctWrong = document.getElementById("correct-wrong");
+var compare = document.querySelector(".compare");
+var hr = document.querySelector(".hr");
+var highScores = document.getElementById("highscores-box");
+var displayScore = document.querySelector(".displayScore");
+var scoresList = document.getElementById("scores-list");
 
 // Quiz questions
-var questions = [
+var questionList = [
   {
-    title:
+    question:
       "1. In JavaScript, a loop that never ends is referred to as a(n)_______.",
     options: [
       "A. While loop",
@@ -18,7 +27,7 @@ var questions = [
     answer: "D. Infinite loop",
   },
   {
-    title:
+    question:
       "2. A very useful tool used during development and debugging for printing content to the debugger is:",
     options: [
       "A. JavaScript",
@@ -29,7 +38,7 @@ var questions = [
     answer: "D. console.log",
   },
   {
-    title:
+    question:
       "3. In CSS, which of the following selector matches a element based on its class attribute?",
     options: [
       "A. The Class Selector",
@@ -40,133 +49,176 @@ var questions = [
     answer: "A. The Class Selector",
   },
   {
-    title:
+    question:
       "4. _______ is the process of finding errors and fixing them within a program.",
     options: ["A. Scanning", "B. Compiling", "C. Debugging", "D. Executing"],
     answer: "C. Debugging",
   },
   {
-    title:
+    question:
       "5. In JavaScript, string values must be enclosed within ____ when being assigned to variables.",
     options: ["A. Commas", "B. Curly brackets", "C. Quotes", "D. Parenthesis"],
     answer: "C. Quotes",
   },
 ];
-// console.log(questions.options)
 
-// Timer starts at 75 seconds when user clicks on start quiz button
-var secondsLeft = 75;
-var holdInterval = 0;
-var penalty = 10;
-
-// Starts quiz questions and timer on click of "start quiz" button
+//When user clicks Start Quiz button, timer and quiz starts together
 startButton.addEventListener("click", function () {
-  if (holdInterval <= 0) {
-    holdInterval = setInterval(function () {
-      secondsLeft--;
-      timerEl.textContent = "Timer:  " + secondsLeft + " " + "seconds";
-      if (secondsLeft <= 0) {
-        clearInterval(holdInterval);
-        finish();
-      }
-    }, 1000);
-  }
-  render(questIndex);
+  questionBox.style.display = "block";
+  startBox.style.display = "none";
+  // console.log(questionList[0].question)
+
+  startQuiz();
+  startTimer();
+  // console.log(questionList[0].options[0])
 });
-// console.log(timerEl)
 
-// Displays questions on the page
-var questIndex = 0;
-var score = 0;
-var olCreate = document.createElement("ol");
+// Timer
+var secondsLeft = 75;
+var currentIndex = 0;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var penalty = 10;
+let timerInterval
+var score = 0
 
-function render(questIndex) {
-  questionsList.innerHTML = "";
-  olCreate.innerHTML = "";
+function startTimer() {
+   timerInterval = setInterval(function () {
+    secondsLeft--;
+    
 
-  for (var i = 0; i < questions.length; i++) {
-    // console.log(questions[0].options)
-    var userQuestion = questions[questIndex].title;
-    var userOptions = questions[questIndex].options;
-    questionsList.textContent = userQuestion;
-  }
-  userOptions.forEach(function (newItem) {
-    var liItem = document.createElement("li");
-    liItem.textContent = newItem;
-    questionsList.appendChild(olCreate);
-    olCreate.appendChild(liItem);
-    liItem.addEventListener("click", compare);
-  });
-}
+    if (secondsLeft <= 0) {
+      clearInterval(timerInterval);
 
-// comparison user options: Correct or Wrong
-function compare(event) {
-  var compareAnswer = event.target;
-
-  if (compareAnswer.matches("li")) {
-    var divCreate = document.createElement("div");
-    divCreate.setAttribute("id", "divCreate");
-    if (compareAnswer.textContent === questions[questIndex].answer) {
-      score++;
-      divCreate.textContent = "Correct!";
-    } else {
-      score--;
-      secondsLeft = secondsLeft - penalty;
-      divCreate.textContent = "Wrong!";
+        questionBox.style.display = "none";
+        finish.style.display = "block";
+        compare.innerHTML = `You got ${correctAnswers} correct and ${wrongAnswers} wrong answers.`;
+        displayScore.innerHTML = `Score: ${score + secondsLeft}`;
+        secondsLeft = 0;
     }
-  }
-
-  // Determines user is on which question number
-  questIndex++;
-
-  if (questIndex >= questions.length) {
-    finish();
-  } else {
-    render(questIndex);
-  }
-  questionsList.appendChild(divCreate);
+    timerEl.textContent = "Time: " + secondsLeft + " " + "seconds";
+  }, 1000);
 }
 
-// End of Questions
-function finish() {
-  questionsList.innerHTML = "";
-  timerEl.innerHTML = "";
-
-  // Finish!
-  var H2 = document.createElement("h2");
-  H2.setAttribute("id", "H2");
-  H2.textContent = "Finish!";
-  questionsList.appendChild(H2);
-
-  var P = document.createElement("p");
-  P.setAttribute("id", "P");
-  questionsList.appendChild(P);
-
-  // Final score
-  if (secondsLeft >= 0) {
-    var remainingTime = secondsLeft;
-    var createP2 = document.createElement("p");
-    clearInterval(holdInterval);
-    P.textContent = "Your final score is: " + remainingTime;
-    // add final score
-    questionsList.appendChild(createP2);
+// Quiz
+function startQuiz() {
+  // console.log(currentIndex);
+  if (currentIndex === questionList.length) {
+    questionBox.style.display = "none";
+    finish.style.display = "block";
+    
+    compare.innerHTML = `You got ${correctAnswers} correct! and ${wrongAnswers} wrong!`;
+    displayScore.innerHTML = `Score: ${score + secondsLeft}`;
+    clearInterval(timerInterval);
+    return;
   }
-// Submit Initials
-  var initialLabel = document.createElement("label");
-  initialLabel.setAttribute("id", "createLabel");
-  initialLabel.textContent = "Enter your initials: ";
-  // add placeholder
-  questionsList.appendChild(initialLabel);
 
-  var textInput = document.createElement("input");
-  textInput.setAttribute("type", "text");
-  textInput.setAttribute("id", "initials");
-  textInput.textContent = "";
-  questionsList.appendChild(textInput);
+  optionButtons.innerHTML = "";
+  for (var i = 0; i < questionList[currentIndex].options.length; i++) {
+    var optbtn = document.createElement("button");
+    optbtn.classList.add("bstyle");
+    optbtn.classList.add("option-btns");
+    optbtn.innerText = questionList[currentIndex].options[i];
+    optionButtons.appendChild(optbtn);
+  }
+  document.querySelectorAll(".option-btns").forEach(function (each) {
+    each.addEventListener("click", function (event) {
+      var chosenOption = event.target.innerText;
+      // console.log(chosenOption);
+      // console.log(questionList[currentIndex].answer);
+      if (chosenOption === questionList[currentIndex].answer) {
+        // console.log("Correct!")
+        correctAnswers++;
+        correctWrong.textContent = "Correct!";
+      } else {
+        // console.log("Wrong!")
+        wrongAnswers++;
+        correctWrong.textContent = "Wrong!";
+        secondsLeft -= 10;
+        // secondsLeft = secondsLeft -10;
+        // alert("wrong!")
+      }
+      currentIndex++;
+      var lineEl = document.createElement("HR");
+      hr.appendChild(lineEl);
 
-  var buttonSubmit = document.createElement("button");
-  buttonSubmit.setAttribute("type", "submit");
-  buttonSubmit.setAttribute("id", "submit");
-  buttonSubmit.textContent = "submit";
-  questionsList.appendChild(buttonSubmit);
+      startQuiz();
+      //timeouts the correct/wrong text and hr tag
+      setTimeout(function () {
+        correctWrong.textContent = "";
+        hr.removeChild(lineEl);
+      }, 1000);
+    });
+  });
+
+  document.getElementById("quiz-question").innerHTML =
+    questionList[currentIndex].question;
 }
+
+document.querySelector(".view-highscore").addEventListener("click", function () {
+    finish.style.display = "none";
+    questionBox.style.display = "none";
+    startBox.style.display = "none";
+    timerEl.style.display = "none";
+    highScores.style.display = "block";
+
+    var finalScoreLocal = JSON.parse(localStorage.getItem("initials"));
+    // console.log(finalScoreLocal);
+
+    finalScoreLocal = finalScoreLocal.sort((a, b) =>{
+      return b.score - a.score
+    }) 
+
+    scoresList.innerHTML = "";
+    for (let i = 0; i < finalScoreLocal.length; i++) {
+      var highscoreEl = document.createElement("li");
+      highscoreEl.innerText = `${finalScoreLocal[i].initials} - ${finalScoreLocal[i].score}`;
+      // console.log(highscoreEl);
+      scoresList.appendChild(highscoreEl);
+    }
+
+  });
+
+  // Local storage
+  var addInitials = document.getElementById("add-initials");
+
+addInitials.addEventListener("submit", function (event) {
+  event.preventDefault();
+  var initials = document.getElementById("initials").value;
+ score = correctAnswers;
+  // console.log(initials, score);
+  var finalScoreLocal = [];
+  
+  if (localStorage.getItem("initials")) {
+    var finalScoreLocal = JSON.parse(localStorage.getItem("initials"));
+  }
+  // console.log(score)
+  // console.log(secondsLeft)
+    finalScoreLocal.push({
+      initials: initials,
+      score: score + secondsLeft,
+  });
+  localStorage.setItem("initials", JSON.stringify(finalScoreLocal));
+
+  finalScoreLocal = finalScoreLocal.sort((a, b) =>{
+    return b.score - a.score
+  }) 
+    
+  finish.style.display = "none";
+  highScores.style.display = "block";
+  document.querySelector("#scores-list").innerHTML = "";
+    for (let i = 0; i < finalScoreLocal.length; i++) {
+      var highscoreEl = document.createElement("li");
+      highscoreEl.innerText = `${finalScoreLocal[i].initials} - ${finalScoreLocal[i].score}`;
+      // console.log(highscoreEl);
+      document.querySelector("#scores-list").appendChild(highscoreEl);
+    }
+ });
+
+ // Clear Highscores list
+ var clearScores = document.getElementById("clear-scores");
+
+ clearScores.addEventListener("click", function (){
+  scoresList.style.display = "none";
+    localStorage.clear();
+});
